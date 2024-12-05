8253f5989d587645f7ef0262ee66e38b416fb408
jeromequandalle <jquandalle@simplon.co>
2024-12-05 13:03:21 +0100

    Update 01_create_debian.tf

--- 01_create_debian.tf
+++ 01_create_debian.tf
@@ -1,7 +1,7 @@
 resource "proxmox_vm_qemu" "debian_test" {
   name       = "debiantest"
   #agent      = 1
-  target_node= "forza"   # Spécifiez le nom de votre nœud Proxmox ici
+  target_node= var.node # Spécifiez le nom de votre nœud Proxmox ici
   clone      = "debian.template"
   scsihw     = "virtio-scsi-pci"
   full_clone   = true
