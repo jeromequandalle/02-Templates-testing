b8f52b0cfad462552a63f66c0c199886f0d0dcd8
jeromequandalle <jquandalle@simplon.co>
2024-12-05 13:05:15 +0100

    Update 02_create_alma.tf

--- 02_create_alma.tf
+++ 02_create_alma.tf
@@ -1,7 +1,7 @@
 resource "proxmox_vm_qemu" "alma_test" {
   name       = "almatest"
   #agent      = 1
-  target_node= "forza"   # Spécifiez le nom de votre nœud Proxmox ici
+  target_node= var.node  # Spécifiez le nom de votre nœud Proxmox ici
   clone      = "alma.template"
   scsihw     = "virtio-scsi-pci"
   full_clone   = true
